6b8712ec10bb9b5a903a58b4f70998989a9e6de6
Alex McKean <amckean@me.com>
2018-03-19 18:49:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,6 +87,20 @@ class Student
    end
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      ORDER BY students.grade
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+     self.new_from_db(row)
+   end 
+  end 
+  
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
